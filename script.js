5607c68ba5ab799e86c4e52ddb7df23bc6bbe7ca
Baldezhnik232 <stanislav.bondarev.2015@gmail.com>
2024-06-06 18:23:37 +0300

    изменения

--- script.js
+++ script.js
@@ -14,12 +14,37 @@ const personalMovieDB = {
     privat: false
 };
 
-const a =  prompt('Один из последних просмотренных фильмов?',''),
-     b = prompt('На сколько оцените его?', ''),
-    c = prompt('Как звали актера ?', ''),
-    d = prompt('Какое имя у главного героя?', '');
+    for (let i = 0; i<1; i++) {
+       const a =  prompt('Один из последних просмотренных фильмов?','');
+        if ( a != null && a != '' && a < 50 ){
+        personalMovieDB.actors[a];
+        console.log('done');
+        }
+        else{
+            console.log('error');
+        }
+    }
 
-    personalMovieDB.actors[a] = b;
-    personalMovieDB.actors[c] = d;
+    for (let j = 0; j<1; j++) {
+        const b = prompt('На сколько оцените его?', '')
+        if (b != null && b != '' && b < 50){
+            personalMovieDB.actors[b];
+            console.log('done');
+        }
+        else{
+            console.log('error');
+        }
+    }
+        if (personalMovieDB.count < 10) {
+            console.log('Просмотрено довольно мало фильмов')
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            console.log('Вы классический зритель')
+        } else if (personalMovieDB.count >= 30) {
+            console.log('Вы киноман')
+        } else {
+            console.log('Произошла ошибка')
+        }
 
+    
     console.log(personalMovieDB);
+    
